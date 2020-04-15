1d95c2f0282538e6fbb94db3710ca833b5fbf193
LucasVanderHaegen <lucas.vanderhaegen@student.hogent.be>
2020-04-15 16:46:03 +0200

    populateDB in commentaar Passwordfield added

--- src/gui/GebruikersSchermController.java
+++ src/gui/GebruikersSchermController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -68,7 +69,8 @@ public class GebruikersSchermController extends AnchorPane {
 	private Label lblWachtwoord;
 
 	@FXML
-	private TextField inputWachtwoord;
+//	private TextField inputWachtwoord;
+	private PasswordField  inputWachtwoord;
 
 	private GebruikerController dc;
 
