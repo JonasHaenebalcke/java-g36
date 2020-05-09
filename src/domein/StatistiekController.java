7435f2c2d6b2836c2fe03f0fcead032792c2e3a3
Jonas Haenebalcke <jonas.haenebalcke@student.hogent.be>
2020-05-09 16:45:11 +0200

    StatistiekController geschreven (zonder te testen)

--- src/domein/StatistiekController.java
+++ src/domein/StatistiekController.java
@@ -1,5 +1,39 @@
 package domein;
 
+import java.util.Arrays;
+
 public class StatistiekController {
 
+	public StatistiekController() {
+	}
+
+	public int geefAantalKeerAanwezig(Gebruiker gebruiker) {
+		int ret = 0;
+
+		for (GebruikerSessie gs : gebruiker.getGebruikerSessieLijst()) {
+			if (gs.isAanwezig())
+				ret++;
+		}
+
+		return ret;
+	}
+
+	public int geefAantalKeerAfwezig(Gebruiker gebruiker) {
+		return gebruiker.getAantalKeerAfwezig();
+	}
+
+	public int geefAantalAanwezigen(Sessie sessie) {
+		int ret = 0;
+
+		for (GebruikerSessie gs : sessie.getGebruikerSessieLijst()) {
+			if (gs.isAanwezig())
+				ret++;
+		}
+
+		return ret;
+	}
+
+	public int geefGemiddeldeScore(Sessie sessie) {
+		return sessie.geefGemiddeldeScore();
+	}
 }
